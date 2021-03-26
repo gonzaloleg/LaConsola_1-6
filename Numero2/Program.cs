1b76d38288b2828d227e287a21b4707f132d97fd
gonzaloleg <70615605+gonzaloleg@users.noreply.github.com>
2021-03-26 14:26:15 -0300

    .

---
+++ Numero2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingreso;
+            Console.WriteLine("Ingrese texto:");
+            ingreso = Console.ReadLine();
+            Console.WriteLine($"Usted ingreso {ingreso}");
+            Console.ReadKey();
+        }
+    }
+}
